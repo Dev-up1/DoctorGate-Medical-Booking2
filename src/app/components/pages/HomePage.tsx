43071a923ca99fc5c6dc9515f2369e584bd016a7
Dev-up1 <electrosign0@gmail.com>
2026-02-05 03:54:14 +0300

    Update HomePage.tsx

--- src/app/components/pages/HomePage.tsx
+++ src/app/components/pages/HomePage.tsx
@@ -6,8 +6,7 @@ import { Shield, Users, Award, Calendar, Star, CheckCircle2, ChevronRight, Chevr
 import { useState } from 'react';
 import { Button } from '@/app/components/ui/button';
 import { Badge } from '@/app/components/ui/badge';
-// لا تحتاج لعمل import في الأعلى
-const heroImage = "/4009640b3787a30ed68fac2c0da543aac2cdda75.png";
+const heroImage = "/ea6fb82e-0acc-4e36-a2cc-f02f30626984.png";
 
 interface HomePageProps {
   onNavigate: (page: string, data?: any) => void;
